b69377cb476bb1463962bc3694e0bbd88cc23086
DhruvShah2008 <78470667+DhruvShah2008@users.noreply.github.com>
2021-06-08 10:17:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,25 @@
+var backgroundImg,background;
+var snowImg,snow
+
+function reload(){
+backgroundImg = loadImage("snow3.jpg")
+snowImg = loadImage("snow5.png")
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  background = createSprite(400, 200, 800, 400);
+  background.addImage("snow3.jpg")
+
+  snow = createSprite(400,200,50,50);
+  snow.addImage("snow5.png")
+
+
 }
 
 function draw() {
   background(255,255,255);  
+
   drawSprites();
 }
